2d402a87b9fd2f2bb56b4d302bc26600a373ce32
Mark Kanko <mkanko66@gmail.com>
2019-01-30 19:06:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,9 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  groceries.each do |types, item|  
+#  groceries.each do |types, item|  # Don't need to do this.
+
+#spent far too long assuming without thinking clearly... why do I need to iterate over the groceries hash? All I need to do is return the values of the groceries hash, which is what .values does! Returns the values of each key value pair and .flatten flattens the arrays, or consolidates the different arrays into one flat (not multi-dimensional) array. The solution is one simple line of code below.
   
    groceries.values.flatten 
 
